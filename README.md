babc465eeb5c09b4902b69d1d48ea4c1dd342f5a
AnShen <x@ipy.me>
2025-01-16 14:26:21 +0800

    修复环境变量未生效的问题

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ## Sub Store with docker compose
 
-打开 docker-compose.yaml 改个url就行了
+修改 docker-compose.yaml 中的 URL 和 SUB_STORE_FRONTEND_BACKEND_PATH 的值，然后 docker-compose up -d 运行
+
+默认是 http 访问，建议使用 cloudflare 加速
+
+宿主机 nginx 反代配置文件 参考
+
+```
+server {
+  listen 80;
+  server_name sub.example.com;
+
+
+  location / {
+        proxy_buffering off;
+        proxy_http_version 1.1;
+
+        proxy_set_header Connection "";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_pass http://127.0.0.1:39080;
+  }
+
+}
+```
--- sub-store/files/entrypoint.sh
+++ sub-store/files/entrypoint.sh
@@ -2,21 +2,38 @@
 
 . /etc/profile
 
-# 检查并设置环境变量
-: "${SUB_STORE_BACKEND_SYNC_CRON:=55 23 * * *}"
-export SUB_STORE_BACKEND_SYNC_CRON
 
-# 下面不建议修改，如果你不知道你在做什么的话
-export SUB_STORE_DATA_BASE_PATH=/db
-export SUB_STORE_BACKEND_API_HOST='127.0.0.1'
-export SUB_STORE_BACKEND_API_PORT=3000
+NGINX_CONFIG='/etc/nginx/http.d/default.conf'
 
+# 检查并设置环境变量， 可通过docker 传入
+: "${SUB_STORE_FRONTEND_BACKEND_PATH:=/$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 18)}"
+: "${SUB_STORE_BACKEND_SYNC_CRON:='55 23 * * *'}"
 
-# 输出环境变量用以确认
-echo "SUB_STORE_BACKEND_SYNC_CRON=${SUB_STORE_BACKEND_SYNC_CRON}"
-echo "SUB_STORE_DATA_BASE_PATH=${SUB_STORE_DATA_BASE_PATH}"
-echo "SUB_STORE_BACKEND_API_HOST=${SUB_STORE_BACKEND_API_HOST}"
-echo "SUB_STORE_BACKEND_API_PORT=${SUB_STORE_BACKEND_API_PORT}"
+# 内置变量不建议修改，如果你不知道你在做什么的话
+SUB_STORE_DATA_BASE_PATH=/db
+SUB_STORE_BACKEND_API_HOST='127.0.0.1'
+SUB_STORE_BACKEND_API_PORT=3001
 
+if grep -q "SUB_STORE_FRONTEND_BACKEND_PATH" "${NGINX_CONFIG}"
+then
+    nginx_prefix_path=$(echo "${SUB_STORE_FRONTEND_BACKEND_PATH}" | tr -d '/')
+    echo "update SUB_STORE_FRONTEND_BACKEND_PATH to ${SUB_STORE_FRONTEND_BACKEND_PATH}"
+    sed -i "s/SUB_STORE_FRONTEND_BACKEND_PATH/${nginx_prefix_path}/g" ${NGINX_CONFIG}
+fi
 
+BACKEND_ADDR="${SUB_STORE_BACKEND_API_HOST}:${SUB_STORE_BACKEND_API_PORT}"
+if grep -q "BACKEND_ADDR" "${NGINX_CONFIG}"
+then
+    echo "update BACKEND_ADDR to ${BACKEND_ADDR}"
+    sed -i "s#BACKEND_ADDR#${BACKEND_ADDR}#g" ${NGINX_CONFIG}
+fi
+
+{
+    echo "export SUB_STORE_BACKEND_SYNC_CRON=${SUB_STORE_BACKEND_SYNC_CRON}"
+    echo "export SUB_STORE_DATA_BASE_PATH=${SUB_STORE_DATA_BASE_PATH}"
+    echo "export SUB_STORE_BACKEND_API_HOST=${SUB_STORE_BACKEND_API_HOST}"
+    echo "export SUB_STORE_BACKEND_API_PORT=${SUB_STORE_BACKEND_API_PORT}"
+} > /app/.env
+
+. /app/.env
 supervisord -c /etc/supervisord.conf
